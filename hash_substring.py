e98c4d03e27c13c89644010fa908ff77008fbb71
Nikita <124347632+Endizzy@users.noreply.github.com>
2023-04-16 23:06:34 +0300

    try 1

--- hash_substring.py
+++ hash_substring.py
@@ -9,24 +9,67 @@ def read_input():
     # read two lines 
     # first line is pattern 
     # second line is text in which to look for pattern 
+    command = input().strip().upper()
+    
+    match command:
+        case "I":
+            pattern = input().strip()
+            text = input().strip()
+            return (pattern, text)
+
+        case "F":
+            with open(f"tests/06") as f:
+                lines = f.readlines()
+                return (lines[0].strip(),lines[1].strip())
+        case _:
+            print("input error")
     
     # return both lines in one return
     
     # this is the sample return, notice the rstrip function
     return (input().rstrip(), input().rstrip())
 
-def print_occurrences(output):
-    # this function should control output, it doesn't need any return
-    print(' '.join(map(str, output)))
+def getHash(text, Weight):
+    tHash = 0
+    for i in range(Weight):
+        tHash +=  ord(text[i]) * pow(10,Weight - i -1)
+
+    return tHash
+
 
 def get_occurrences(pattern, text):
     # this function should find the occurances using Rabin Karp alghoritm 
 
+    pa = len(pattern)
+    te = len(text)
+    occurrences = []
+
+    paHash = getHash(pattern,pa)
+    teHash = getHash(text,te)
+    
+    for i in range(te - pa + 1):
+
+        if paHash == teHash:
+            continue
+
+        if text[i:i+pa] == pattern:
+            occurrences.append(i)
+
+
     # and return an iterable variable
-    return [0]
+    return occurrences
+
+
+
+
+
+def print_occurrences(output):
+    # this function should control output, it doesn't need any return
+    print(' '.join(map(str, output)))
+
+
 
 
 # this part launches the functions
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
-
