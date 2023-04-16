a47455c35d6fe00aaf384fd62b5d4b6b2e21c41c
Nikita <124347632+Endizzy@users.noreply.github.com>
2023-04-16 23:37:28 +0300

    minor fix

--- hash_substring.py
+++ hash_substring.py
@@ -29,12 +29,34 @@ def read_input():
     # this is the sample return, notice the rstrip function
     return (input().rstrip(), input().rstrip())
 
-def getHash(text, Weight):
-    tHash = 0
-    for i in range(Weight):
-        tHash +=  ord(text[i]) * pow(10,Weight - i -1)
 
-    return tHash
+def read_input():
+    # this function needs to aquire input both from keyboard and file
+    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    
+    
+    # after input type choice
+    # read two lines 
+    # first line is pattern 
+    # second line is text in which to look for pattern 
+    command = input().strip().upper()
+    
+    if command == "I":
+        pattern = input().strip()
+        text = input().strip()
+        return (pattern, text)
+
+    elif command == "F":
+        with open(f"tests/06") as f:
+            lines = f.readlines()
+            return (lines[0].strip(),lines[1].strip())
+    else:
+        print("input error")
+    
+    # return both lines in one return
+    
+    # this is the sample return, notice the rstrip function
+    return (input().rstrip(), input().rstrip())
 
 
 def get_occurrences(pattern, text):
@@ -44,29 +66,28 @@ def get_occurrences(pattern, text):
     te = len(text)
     occurrences = []
 
-    paHash = getHash(pattern,pa)
-    teHash = getHash(text,te)
+    paHash = 0
+    teHash = 0
+
+    for i in range(pa):
+        paHash +=  ord(pattern[i])
+        teHash +=  ord(text[i])
 
     for i in range(te - pa + 1):
         if paHash == teHash:
-            continue
+            if text[i:i+pa] == pattern:
+                occurrences.append(i)
 
-        if text[i:i+pa] == pattern:
-            occurrences.append(i)
-
+        if i < te - pa:
+            teHash = teHash - ord(text[i]) + ord(text[i+pa])
 
     # and return an iterable variable
     return occurrences
 
 
-
-
-
 def print_occurrences(output):
     # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
-
-
 
 
 # this part launches the functions
